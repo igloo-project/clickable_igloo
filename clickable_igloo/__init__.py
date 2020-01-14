9957ef95acc8f216877dcc8eaafc7f2e576beb62
Laurent Almeras <laurent.almeras@kobalt.fr>
2020-01-14 18:33:46 +0100

    add new command shortcuts

--- clickable_igloo/__init__.py
+++ clickable_igloo/__init__.py
@@ -10,6 +10,8 @@ import subprocess
 
 import clickable.coloredlogs
 
+import clickable_ansible
+
 clickable.coloredlogs.bootstrap()
 logger = logging.getLogger('stdout.clickable')
 
@@ -74,3 +76,70 @@ TODO: full implementation of dry_run
 def symlink_folders(from_root, to_root, folders=DEFAULT_FOLDERS, dry_run=False):
     for folder in folders:
         symlink_folder(os.path.join(from_root, folder), os.path.join(to_root, folder), dry_run)
+
+
+_PHASES = ['firewall', 'httpd', 'tomcat', 'filesystem', 'postgresql', 'glowroot', 'tomcat']
+def _igloo_full_decorators():
+    # function that handles listed options below
+    def configure_extra_vars():
+        def wrapper(*args, **kwargs):
+            extra_vars = kwargs['extra_vars']
+            if not extra_vars:
+                extra_vars = []
+            new_kwargs = dict(kwargs)
+
+            deploy_war = False
+
+            # deliver a war file
+            if 'deliver' in new_kwargs:
+                deliver = new_kwargs.pop('deliver')
+                extra_vars.append('{}={}'.format('playbook_gitlab_cd_war_location', deliver))
+                deploy_war = True
+
+            # build and deliver application
+            if new_kwargs.get('build_igloo', None):
+                extra_vars.append('{}={}'.format('playbook_build_igloo', 'true'))
+            if new_kwargs.get('build_project', None) or new_kwargs.get('build_igloo', None):
+                extra_vars.append('{}={}'.format('playbook_build_project', 'true'))
+                deploy_war = True
+            new_kwargs.pop('build_project')
+            new_kwargs.pop('build_igloo')
+
+            # force rebuild
+            if new_kwargs.pop('force_rebuild', None):
+                extra_vars.append('{}={}'.format('playbook_rebuild_project', 'true'))
+                extra_vars.append('{}={}'.format('playbook_rebuild_igloo', 'true'))
+            
+            phases = new_kwargs.pop('phases', None)
+            if phases:
+                for phase in phases:
+                    extra_args.append('--tags={}'.format(phase))
+
+            skip_handlers = new_kwargs.pop('skip_handlers', None)
+            if skip_handlers:
+                extra_vars.append('{}={}'.format('handlers_inhibited', 'true'))
+
+            # playbook_deploy_war=true if war or built application must be delivered
+            if deploy_war:
+                extra_vars.append('{}={}'.format('playbook_deploy_war', 'true'))
+            else:
+                extra_vars.append('{}={}'.format('playbook_deploy_war', 'false'))
+    return [
+        click.option('--deliver', default=None, help='War to deliver. If none/empty, delivery is not performed.'),
+        click.option('--build-project', default=False, is_flag=True, help='Build project and deliver war.'),
+        click.option('--build-igloo', default=False, is_flag=True, help='Build igloo; implies build-project.'),
+        click.option('--force-rebuild', default=False, is_flag=True, help='Rebuild already built items.'),
+        click.option('--skip-handlers', default=False, is_flag=True, help='Skip handlers; display messages instead. YOU MUST ensure manually that needed restart are performed.'),
+        click.option('--phases', default='', help='Choose phases to perform, separate with comma ",": ' + ', '.join(_PHASES))
+    ]
+
+
+def run_igloo_full_task(click_group, name, playbook, static_extra_vars=[],
+        decorators=[], help=None, short_help=None,
+        common_hosts=False):
+    merged_decorators = []
+    merged_decorators.extend(decorators)
+    merged_decorators.extend(_igloo_full_decorators())
+    return clickable_ansible.run_playbook_task(click_group, name, playbook,
+            static_extra_vars=static_extra_vars, decorators=merged_decorators,
+            help=help, short_help=short_help, common_hosts=common_hosts)
